74e9a6a6aafde4a73b75441bef2ae9b9e17f06c9
MiraSkold <mira.skold@elev.ga.lbs.se>
2026-01-09 11:17:39 +0100

    Main Menu

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange: MonoBehaviour
+{
+    public string scene;
+    public void Play()
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Game is quitting...");
+        Application.Quit();
+
+
+    }
+
+}
